add212d6b0bbf98ffaa0c296e92cb53d8e218b58
deezy13 <deezy13@gmail.com>
2019-09-08 00:53:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -16,4 +16,7 @@ def find_min_value(array)
    min_value_in_array = array.min
 end
 
-find_min_value
+find_max_value(valley)
+find_element_index(8)
+find_min_value(valley)
+valley = [5,4,3,2,1,0,1,2,3,4,5]
