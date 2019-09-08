2d40b1073c84489d3039cd4cb0e208f82920845c
deezy13 <deezy13@gmail.com>
2019-09-08 00:49:38 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,19 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
-end
+ 
+ index_of_value = array.index(value_to_find)
+ puts index_of_value
+ return index_of_value
+  
+ end
 
 def find_max_value(array)
   # Add your solution here
+  
+  
 end
 
 def find_min_value(array)
   # Add your solution here
 end
+
