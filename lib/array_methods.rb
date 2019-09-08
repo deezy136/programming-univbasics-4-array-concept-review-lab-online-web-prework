08f0b513324474511bb34090fcd269060e90fb3f
deezy13 <deezy13@gmail.com>
2019-09-08 00:55:46 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,23 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
-end
+   index_of_value = array.index(value_to_find)
+ puts index_of_value
+ return index_of_value
+   end
 
 def find_max_value(array)
-  # Add your solution here
+     max_value_in_array = array.max
+  puts max_value_in_array
+  return max_value_in_array
 end
+
 
 def find_min_value(array)
   # Add your solution here
+   min_value_in_array = array.min
 end
+
+
+valley = [5,4,3,2,1,0,1,2,3,4,5]
+find_max_value(valley)
+find_element_index(valley, 9)
+find_min_value(valley)
